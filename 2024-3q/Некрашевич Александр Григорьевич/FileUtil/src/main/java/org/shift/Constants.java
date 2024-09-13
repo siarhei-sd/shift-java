9c2e1c5758bc09d53be9f6eb77245607c3d8fce3
Malaenko Maksim <m.malaenko@cft.ru>
2024-09-13 17:38:45 +0300

    2024-3q Некрашевич Александр Григорьевич

---
+++ 2024-3q/Некрашевич Александр Григорьевич/FileUtil/src/main/java/org/shift/Constants.java
@@ -0,0 +1,22 @@
+package org.shift;
+
+public class Constants {
+    public static final String INTEGERS_TXT = "integers.txt";
+    public static final String FLOATS_TXT = "floats.txt";
+    public static final String STRINGS_TXT = "strings.txt";
+    public static final String FILE_NOT_FOUND_MSG = " wasn't found. Skipping it.";
+    public static final String PARSING_CMD_OPTION_FAILED_MSG = "Parsing failed.  Reason: ";
+    public static final String FILE_WAS_NOT_WRITTEN_MSG = "File wasn't written: ";
+    public static final String SHORT_STAT_TEMPLATE = "; shortest = %d; longest = %d";
+    public static final String NUMBERS_COUNT_LABEL = " elements = ";
+    public static final String SHORT_STAT_LABEL = "short statistic";
+    public static final String FULL_STAT_LABEL = "full statistic";
+    public static final String INT_STAT_TEMPLATE = "; min = %d; max = %d sum = %d; avg = %f;";
+    public static final String FLOAT_STAT_TEMPLATE = "; min = %s; max = %s sum = %s; avg = %s;";
+
+    public static final String O_OPTION_DESCRIPTION = "Path for result files.";
+    public static final String P_OPTION_DESCRIPTION = "Prefix for result files.";
+    public static final String A_OPTION_DESCRIPTION = "Append to existing files.";
+    public static final String S_OPTION_DESCRIPTION = "Short statistic.";
+    public static final String F_OPTION_DESCRIPTION = "Full statistic.";
+}
